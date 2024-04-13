59e7da4f41e6824c9a10fba870e9f56bd712e717
Pahan99 <isurupahan321@gmail.com>
2024-04-14 02:17:36 +0530

    Updated app

---
+++ app/app.py
@@ -0,0 +1,34 @@
+import streamlit as st
+
+def main():
+    st.set_page_config(layout="wide")
+    st.title("🧬 Hybrid Sequence Classification")
+
+    # Upload files
+    st.header("Upload Kraken2 Output Files")
+
+    # Divide the screen into two columns
+    col1, col2 = st.columns(2)
+
+    # File uploader for the first file in the first column
+    with col1:
+        uploaded_file_1 = st.file_uploader("Upload Kraken2 Classification Result", type=["txt"])
+
+    # File uploader for the second file in the second column
+    with col2:
+        uploaded_file_2 = st.file_uploader("Upload Kraken2 Report", type=["txt"])
+
+    if uploaded_file_1 and uploaded_file_2:
+        # Button for hybrid classification
+
+        if st.button("Perform Hybrid Classification"):
+            # Add code here to perform hybrid classification
+            st.header("Performing Hybrid Classification...")
+            # You can use the uploaded files for hybrid classification
+
+            # Display results
+            st.header("Results")
+
+
+if __name__ == "__main__":
+    main()

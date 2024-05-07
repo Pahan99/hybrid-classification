24c6a22226aac445eba5c9ae962493558df4e04b
HasithaIndrajith <hasitha.19@cse.mrt.ac.lk>
2024-05-08 01:01:17 +0530

    added kbm2 folder

--- app/kbm2.sh
+++ app/kbm2.sh
@@ -5,6 +5,6 @@ OUTPUT_DIR="output"
 exp="./test_data/"
 mkdir -p $OUTPUT_DIR
 grep ">" $exp/reads.fasta > $exp/read_ids
-wtdbg2/kbm2 -i $exp/reads.fasta -d $exp/reads.fasta -n 2000 -l 2560 -t 16 | python3 filter_alignments.py
+kmb2/wtdbg2/kbm2 -i $exp/reads.fasta -d $exp/reads.fasta -n 2000 -l 2560 -t 16 | python3 filter_alignments.py
 
 echo "Finished kbm2"
